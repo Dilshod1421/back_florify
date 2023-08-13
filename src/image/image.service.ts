8eae3936ed0018d153495b5d4858b6c0e3b9e2dc
Ismoil4432 <ismoil4432@gmail.com>
2023-08-13 13:48:10 +0500

    image upload added

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,99 +1,88 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
-import { Image } from './models/image.model';
-import { ImageDto } from './dto/image.dto';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { Storage } from '@google-cloud/storage';
+import { extname } from 'path';
+
+const storage = new Storage({
+  projectId: 'upload-image-392818',
+  keyFilename: 'keyfile.json',
+});
+
+const bucketName = 'upload-image-nest-florify';
+const bucket = storage.bucket(bucketName);
 
 @Injectable()
 export class ImageService {
-  constructor(
-    @InjectModel(Image)
-    private readonly imageRepository: typeof Image,
-  ) {}
+  async create(image: Express.Multer.File) {
+    if (!image) throw new BadRequestException('No image');
 
-  async create(imageDto: ImageDto) {
     try {
-      const image = await this.imageRepository.create(imageDto);
-      return { message: "Rasm qo'shildi", image };
+      const fileName =
+        (await this.generateUniqueFileName()) + extname(image.originalname);
+      const file = bucket.file(fileName);
+      await file.save(image.buffer, { resumable: false });
+      return fileName;
     } catch (error) {
-      throw new BadRequestException(error.message);
+      throw new HttpException(
+        'Error with uploading images',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
   async findAll() {
-    try {
-      const images = await this.imageRepository.findAll({
-        include: { all: true },
-      });
-      return images;
-    } catch (error) {
-      throw new BadRequestException(error.message);
+    const [files] = await bucket.getFiles();
+    const allFileNames = files.map((file) => file.name);
+
+    return allFileNames;
+  }
+
+  async findOne(fileName: string, res: Response) {
+    const file = bucket.file(fileName);
+    const exists = await file.exists();
+    if (!exists[0]) {
+      throw new HttpException('Image not found', HttpStatus.NOT_FOUND);
+    }
+
+    const stream = file.createReadStream();
+    stream.pipe(res);
+  }
+
+  async remove(fileName: string) {
+    const file = bucket.file(fileName);
+    const exists = await file.exists();
+    if (exists[0]) {
+      await file.delete();
+      return fileName;
+    } else {
+      throw new HttpException('Image not found', HttpStatus.NOT_FOUND);
     }
   }
 
-  async paginate(page: number) {
-    try {
-      page = Number(page);
-      const limit = 10;
-      const offset = (page - 1) * limit;
-      const images = await this.imageRepository.findAll({
-        include: { all: true },
-        offset,
-        limit,
-      });
-      const total_count = await this.imageRepository.count();
-      const total_pages = Math.ceil(total_count / limit);
-      const res = {
-        status: 200,
-        data: {
-          records: images,
-          pagination: {
-            currentPage: page,
-            total_pages,
-            total_count,
-          },
-        },
-      };
-      return res;
-    } catch (error) {
-      throw new BadRequestException(error.message);
-    }
+  async generateFileName() {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const prefix =
+      letters.charAt(Math.floor(Math.random() * letters.length)) +
+      letters.charAt(Math.floor(Math.random() * letters.length)) +
+      letters.charAt(Math.floor(Math.random() * letters.length));
+    const suffix = Math.floor(Math.random() * 90000) + 10000;
+    return prefix + suffix;
   }
 
-  async findById(id: string) {
-    try {
-      const image = await this.imageRepository.findOne({
-        where: { id },
-        include: { all: true },
-      });
-      if (!image) {
-        throw new BadRequestException('Rasm topilmadi!');
-      }
-      return image;
-    } catch (error) {
-      throw new BadRequestException(error.message);
-    }
-  }
+  async generateUniqueFileName() {
+    const [files] = await bucket.getFiles();
+    const allUniqueFileNames = files.map((file) => file.name);
 
-  async update(id: string, imageDto: ImageDto) {
-    try {
-      const image = await this.findById(id);
-      const updated_info = await this.imageRepository.update(imageDto, {
-        where: { id: image.id },
-        returning: true,
-      });
-      return { message: 'Rasm tahrirlandi', image: updated_info[1][0] };
-    } catch (error) {
-      throw new BadRequestException(error.message);
+    let fileName: any;
+    while (true) {
+      fileName = await this.generateFileName();
+      if (!allUniqueFileNames.includes(fileName)) break;
     }
-  }
-
-  async remove(id: string) {
-    try {
-      const image = await this.findById(id);
-      image.destroy();
-      return { message: "Rasm o'chirildi" };
-    } catch (error) {
-      throw new BadRequestException(error.message);
-    }
+    return fileName;
   }
 }
