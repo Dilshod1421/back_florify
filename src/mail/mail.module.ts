5f8f66929eb4a3780a591306c6f9990ab9cfca6f
Dilshod1421 <dilshod7861@gmail.com>
2023-11-28 19:23:10 +0500

    mail on comment

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,38 +1,38 @@
-import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { Module } from '@nestjs/common';
-import { MailService } from './mail.service';
-import { join } from 'path';
-import { ConfigService } from '@nestjs/config';
+// import { MailerModule } from '@nestjs-modules/mailer';
+// import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+// import { Module } from '@nestjs/common';
+// import { MailService } from './mail.service';
+// import { join } from 'path';
+// import { ConfigService } from '@nestjs/config';
 
-@Module({
-  imports: [
-    MailerModule.forRootAsync({
-      useFactory: async (config: ConfigService) => ({
-        transport: {
-          host: config.get('MAILER_HOST'),
-          secure: true,
-          auth: {
-            user: config.get('MAILDEV_USER'),
-            pass: config.get('MAILDEV_PASS'),
-          },
-        },
-        defaults: {
-          from: `"Main Stream" ${config.get('MAILDEV_USER')}`,
-        },
-        template: {
-          dir: join(__dirname, 'templates'),
-          adapter: new HandlebarsAdapter(),
-          template: 'confirmation',
-          options: {
-            strict: true,
-          },
-        },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
-  providers: [MailService],
-  exports: [MailService],
-})
-export class MailModule {}
+// @Module({
+//   imports: [
+//     MailerModule.forRootAsync({
+//       useFactory: async (config: ConfigService) => ({
+//         transport: {
+//           host: config.get('MAILER_HOST'),
+//           secure: true,
+//           auth: {
+//             user: config.get('MAILDEV_USER'),
+//             pass: config.get('MAILDEV_PASS'),
+//           },
+//         },
+//         defaults: {
+//           from: `"Main Stream" ${config.get('MAILDEV_USER')}`,
+//         },
+//         template: {
+//           dir: join(__dirname, 'templates'),
+//           adapter: new HandlebarsAdapter(),
+//           template: 'confirmation',
+//           options: {
+//             strict: true,
+//           },
+//         },
+//       }),
+//       inject: [ConfigService],
+//     }),
+//   ],
+//   providers: [MailService],
+//   exports: [MailService],
+// })
+// export class MailModule {}
