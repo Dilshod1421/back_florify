01f914e2b133a10351d885f2dce2535393f245c0
Dilshod1421 <dilshod7861@gmail.com>
2023-12-23 23:27:59 +0500

    product present

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -232,7 +232,9 @@ export class ProductService {
         offset,
         limit,
       });
-      const total_count = await this.productRepository.count();
+      const total_count = await this.productRepository.count({
+        where: { date },
+      });
       const total_pages = Math.ceil(total_count / limit);
       const response = {
         status: HttpStatus.OK,
